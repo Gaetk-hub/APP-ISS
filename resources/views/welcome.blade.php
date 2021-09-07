d83b968cac8b24c7f49d36dd429d97ca0411032d
gaetk <gaetankulemfuka08@gmail.com>
2021-09-07 07:19:26 +0200

    Deploiement de la page d accueil de l ISS

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,223 @@
+<!DOCTYPE HTML>
+<!--
+	Verti by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>ISS</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{{asset('assets/css/main.css')}}" />
+
+    <link rel="shortcut icon" href="{{('images/logoiss.ico')}}">
+	</head>
+	<body class="is-preload homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<header id="header" class="container">
+
+						<!-- Logo -->
+							<div id="logo">
+								<h1><a href="#"> <img src="{{asset('images/logoiss.PNG')}}" alt="logo" style="width: 50px;"></a></h1>
+								<span>INSTITUT SUPERIEUR DE STATISTIQUES DE KINSHASA</span>
+							</div>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li class="current"><a href="index.html">Welcome</a></li>
+								</ul>
+							</nav>
+
+					</header>
+				</div>
+
+			<!-- Banner -->
+				<div id="banner-wrapper">
+					<div id="banner" class="box container">
+						<div class="row">
+							<div class="col-7 col-12-medium">
+								<h2>Bienvenue à l'ISS</h2>
+								<p>Institut supérieur de statistiques de Kinshasa</p>
+							</div>
+							<div class="col-5 col-12-medium">
+								<ul>
+									<li><a href="#" class="button large icon solid fa-arrow-circle-right">Espace Etudiant</a></li>
+									<li><a href="{{('images/inscription.jpg')}}" class="button alt large icon solid fa-question-circle">Inscriptions</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Features -->
+				<div id="features-wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="{{asset('images/pic01.jpg')}}" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>Option 1</h2>
+												<p>Maybe here as well I think</p>
+											</header>
+											<p>Phasellus quam turpis, feugiat sit amet in, hendrerit in lectus. Praesent sed semper amet bibendum tristique fringilla.</p>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="{{asset('images/pic02.jpg')}}" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>Faculté 2</h2>
+												<p>This is also an interesting subtitle</p>
+											</header>
+											<p>Phasellus quam turpis, feugiat sit amet in, hendrerit in lectus. Praesent sed semper amet bibendum tristique fringilla.</p>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-4 col-12-medium">
+
+								<!-- Box -->
+									<section class="box feature">
+										<a href="#" class="image featured"><img src="{{asset('images/pic03.jpg')}}" alt="" /></a>
+										<div class="inner">
+											<header>
+												<h2>Faculté 3</h2>
+												<p>Here's another intriguing subtitle</p>
+											</header>
+											<p>Phasellus quam turpis, feugiat sit amet in, hendrerit in lectus. Praesent sed semper amet bibendum tristique fringilla.</p>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row gtr-200">
+							<div class="col-4 col-12-medium">
+
+								<!-- Sidebar -->
+									<div id="sidebar">
+										<section class="widget thumbnails">
+											<h3>Administration</h3>
+											<div class="grid">
+												<div class="row gtr-50">
+													<div class="col-6"><a href="#" class="image fit"><img src="{{asset('images/pic04.jpg')}}" alt="" /></a></div>
+													<div class="col-6"><a href="#" class="image fit"><img src="{{asset('images/pic05.jpg')}}" alt="" /></a></div>
+													<div class="col-6"><a href="#" class="image fit"><img src="{{asset('images/pic06.jpg')}}" alt="" /></a></div>
+													<div class="col-6"><a href="#" class="image fit"><img src="{{asset('images/pic07.jpg')}}" alt="" /></a></div>
+												</div>
+											</div>
+											<a href="#" class="button icon fa-file-alt">Espace</a>
+										</section>
+									</div>
+
+							</div>
+							<div class="col-8 col-12-medium imp-medium">
+
+								<!-- Content -->
+									<div id="content">
+										<section class="last">
+											<h2>POURQUOI CHOISIR DE SE FORMER A L’ISS-KIN</h2>
+<p>L'ISS-KIN est pluridisciplinaire intervient dans six grands champs disciplinaires, forme des Statisticiens, des Documentalistes, des Commercialistes, des Gestionnaires de Transport, des ingénieurs rapidement opérationnels, particulièrement adaptables aux évolutions de la technologie et aux mutations de la société. Nos formations s’appuient sur les activités de recherche et sur la valorisation de l'enseignement avec un objectif clé l'amener à un niveau international!</p>
+                  	</section>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						<div class="row">
+							<div class="col-3 col-6-medium col-12-small">
+
+								<!-- Links -->
+									<section class="widget links">
+										<h3>Qui sommes nous?</h3>
+										<ul class="style2">
+											<li><a href="#"> quis nostrud nderit iloident, sunt in culpa qui officia deserunt mollit anim id est laborum. </a></li>
+
+										</ul>
+									</section>
+
+							</div>
+							<div class="col-3 col-6-medium col-12-small">
+
+								<!-- Links -->
+									<section class="widget links">
+
+									</section>
+
+							</div>
+							<div class="col-3 col-6-medium col-12-small">
+
+								<!-- Links -->
+									<section class="widget links">
+
+									</section>
+
+							</div>
+							<div class="col-3 col-6-medium col-12-small">
+
+								<!-- Contact -->
+									<section class="widget contact last">
+										<h3>Contacts</h3>
+										<ul>
+											<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+											<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+											<li><a href="#" class="icon brands fa-pinterest"><span class="label">Pinterest</span></a></li>
+										</ul>
+										<p>2536 Avenue Blv.Sendwe,<br />
+										 Kalamu, Kinshasa<br />
+										+243 81 767 04 05</p>
+									</section>
+
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12">
+								<div id="copyright">
+									<ul class="menu">
+										<li>&copy; Untitled. All rights reserved</li><li>Design: <a href="https://gaetk.net">Gaetk</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</footer>
+				</div>
+
+			</div>
+
+		<!-- Scripts -->
+
+			<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+			<script src="{{asset('assets/js/jquery.dropotron.min.js')}}"></script>
+			<script src="{{asset('assets/js/browser.min.js')}}"></script>
+			<script src="{{asset('assets/js/breakpoints.min.js')}}"></script>
+			<script src="{{asset('assets/js/util.js')}}"></script>
+			<script src="{{asset('assets/js/main.js')}}"></script>
+
+	</body>
+</html>
